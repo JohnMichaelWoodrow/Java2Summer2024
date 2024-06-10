e388e2160c378c62d9179ba443fc6c668c916487
John-Michael Woodrow <john-michael.woodrow19@ed.cna.nl.ca>
2024-06-10 19:11:11 -0230

    Final commit

--- Assignment3/src/main/java/question4/Redactor.java
+++ Assignment3/src/main/java/question4/Redactor.java
@@ -1,4 +1,44 @@
 package src.main.java.question4;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.regex.*;
+
 public class Redactor {
+    public static void main(String[] args) {
+        String inputFileName = "src/main/java/question4/sampleInfo.txt";
+        String outputFileName = "src/main/java/question4/sampleInfoRedacted.txt";
+        redactFile(inputFileName, outputFileName);
+    }
+
+    private static void redactFile(String inputFileName, String outputFileName) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(redactLine(line)).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
+            writer.write(content.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String redactLine(String line) {
+        // Dates
+        line = line.replaceAll("\\b\\d{2}-\\d{2}-\\d{2}(\\d{2})?\\b", "##-##-####");
+        // Credit Card Numbers
+        line = line.replaceAll("\\b\\d{4}-\\d{4}-\\d{4}-\\d{4}\\b", "####-####-####-####");
+        // Currency
+        line = line.replaceAll("\\$(\\d+(\\.\\d{2})?)", "\\$##.##");
+        // Security Codes
+        line = line.replaceAll("\\bCODE\\w{16}\\b", "CODE################");
+
+        return line;
+    }
 }
