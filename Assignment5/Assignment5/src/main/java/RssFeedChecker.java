e60b5022dd443f8bb8e49c616b5d853f8739cd2d
John-Michael Woodrow <john-michael.woodrow19@ed.cna.nl.ca>
2024-07-22 17:14:30 -0230

    Assignment 5 i think im done

--- Assignment5/Assignment5/src/main/java/RssFeedChecker.java
+++ Assignment5/Assignment5/src/main/java/RssFeedChecker.java
@@ -1,27 +1,63 @@
-import org.w3c.dom.*;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-public class RssFeedChecker implements Runnable {
+/**
+ * RSSFeedChecker class. Implements the Runnable interface and checks an RSS feed URL
+ * for new items every 10 seconds. Keeps track of seen items to avoid repetition.
+ */
+public class RSSFeedChecker implements Runnable {
     private final String feedUrl;
+    private final Set<RSSItem> seenItems;
 
-    public RssFeedChecker(String feedUrl) {
+    /**
+     * Constructs an RSSFeedChecker for a feed URL.
+     *
+     * @param feedUrl the URL of the RSS feed.
+     */
+    public RSSFeedChecker(String feedUrl) {
         this.feedUrl = feedUrl;
+        this.seenItems = new HashSet<>();
     }
 
     @Override
     public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                checkFeed();
+                Thread.sleep(10000); // 10 seconds
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    /**
+     * Checks the RSS feed for new items and prints them if they are new.
+     */
+    public void checkFeed() {
         try {
             URL url = new URL(feedUrl);
+            // XML Document building
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(url.openStream());
 
+            // This is how you work with XML - you do not need to modify this!
             NodeList itemList = doc.getElementsByTagName("item");
-            List<RssItem> items = new ArrayList<>();
+            List<RSSItem> newItems = new ArrayList<>();
 
             for (int i = 0; i < itemList.getLength(); i++) {
                 Node itemNode = itemList.item(i);
@@ -30,14 +66,18 @@ public class RssFeedChecker implements Runnable {
                     String title = itemElement.getElementsByTagName("title").item(0).getTextContent();
                     String link = itemElement.getElementsByTagName("link").item(0).getTextContent();
                     String pubDate = itemElement.getElementsByTagName("pubDate").item(0).getTextContent();
-                    items.add(new RssItem(title, link, pubDate));
+                    RSSItem newItem = new RSSItem(title, link, pubDate);
+                    if (!seenItems.contains(newItem)) {
+                        newItems.add(newItem);
+                        seenItems.add(newItem);
+                    }
                 }
             }
 
             synchronized (System.out) {
                 System.out.println("Feed: " + feedUrl);
-                for (int i = 0; i < Math.min(3, items.size()); i++) {
-                    RssItem item = items.get(i);
+                for (int i = 0; i < Math.min(3, newItems.size()); i++) {
+                    RSSItem item = newItems.get(i);
                     System.out.println("Title: " + item.getTitle());
                     System.out.println("Link: " + item.getLink());
                     System.out.println("Published: " + item.getPubDate());
@@ -48,28 +88,7 @@ public class RssFeedChecker implements Runnable {
             e.printStackTrace();
         }
     }
-
-    private static class RssItem {
-        private final String title;
-        private final String link;
-        private final String pubDate;
-
-        public RssItem(String title, String link, String pubDate) {
-            this.title = title;
-            this.link = link;
-            this.pubDate = pubDate;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getLink() {
-            return link;
-        }
-
-        public String getPubDate() {
-            return pubDate;
-        }
-    }
 }
+
+
+
